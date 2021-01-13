ca95b818ab87a654c95bb2b4dd2f6252dfd47641
Dany <dany8079@gmail.com>
2021-01-12 18:52:27 -0600

    added walking animaions for 4 directions, FPS to 30, and added borders.

--- game.py
+++ game.py
@@ -1,14 +1,26 @@
 import pygame
 import os, sys
 
-# Values
-playerWidth = 16
-playerHeight = 32
+# Values - Player
 playerPosX = 264
 playerPosY = 294
-playerVel = 0.03
-playerAnim = 3
+playerVel = 4
 playerWalkCount = 0
+playerIsLeft = False
+playerIsRight = False
+# 0 = Left, 1 = Right
+playerLastDirection = 1
+
+# Values - Controlled Variables
+playerWidth = 16
+playerHeight = 32
+FPS = 30
+
+# Values - Bounderies
+LeftBorder = 24
+RightBorder = 503 - playerWidth
+TopBorder = 56
+BottomBorder = 615 - playerHeight
 
 
 # Init PyGame module
@@ -22,12 +34,12 @@ pygame.display.set_caption('Debt Collector')
 screen = pygame.display.set_mode((528,640))
 
 
-
-# load data
+# Load data
 drawbg = pygame.image.load('./sprites/bg.png')
 bgmusic = pygame.mixer.music.load('./bgm/song.ogg')
 
-playerStill = pygame.image.load('./sprites/player/playerStill.png')
+playerStillR = pygame.image.load('./sprites/player/playerStillR.png')
+playerStillL = pygame.image.load('./sprites/player/playerStillL.png')
 walkLeft = [pygame.image.load('./sprites/player/playerWalk1L.png'), pygame.image.load('./sprites/player/playerWalk2L.png'),
             pygame.image.load('./sprites/player/playerWalk3L.png'), pygame.image.load('./sprites/player/playerWalk4L.png'),
             pygame.image.load('./sprites/player/playerWalk5L.png'), pygame.image.load('./sprites/player/playerWalk6L.png')]
@@ -38,30 +50,77 @@ walkRight = [pygame.image.load('./sprites/player/playerWalk1R.png'), pygame.imag
 
 
 # Startup (Load bg and play bgm)
-screen.blit(drawbg,(0,0))
 pygame.display.flip()
 pygame.mixer.music.play(loops=-1)
 
-# Draw Window
+# Draw Window & GUI
 def DrawGameWindow():
-    screen.blit(playerStill, (playerPosX,playerPosY))
+    pygame.display.update()
+    screen.blit(drawbg,(0,0))
+
+# Player Character
+def Player():
+    global playerWalkCount
+    # Character Walking animations
+    if playerWalkCount + 1 >= 18:
+        playerWalkCount = 0
+    if playerIsLeft:
+        screen.blit(walkLeft[playerWalkCount//3], (playerPosX, playerPosY))
+        playerWalkCount += 1
+    elif playerIsRight:
+        screen.blit(walkRight[playerWalkCount//3], (playerPosX, playerPosY))
+        playerWalkCount += 1
+    else:
+        if playerLastDirection == 0:
+            screen.blit(playerStillL, (playerPosX, playerPosY))
+        else:
+            screen.blit(playerStillR, (playerPosX, playerPosY))
     pygame.display.update()
 
 # Running
 running = True
 
 while running:
+    clock.tick(FPS)
+
     # Keyboard Controls
-    if pygame.key.get_pressed()[pygame.K_LEFT]:
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT] and playerPosX > LeftBorder:
         playerPosX -= playerVel
-    if pygame.key.get_pressed()[pygame.K_RIGHT]:
+        playerIsLeft = True
+        playerIsRight = False
+        playerLastDirection = 0
+    elif keys[pygame.K_RIGHT] and playerPosX < RightBorder:
         playerPosX += playerVel
-    if pygame.key.get_pressed()[pygame.K_UP]:
+        playerIsLeft = False
+        playerIsRight = True
+        playerLastDirection = 1
+
+    elif keys[pygame.K_UP] and playerPosY > TopBorder:
         playerPosY -= playerVel
-    if pygame.key.get_pressed()[pygame.K_DOWN]:
+        if playerLastDirection == 0:
+            playerIsLeft = True
+            playerIsRight = False
+        elif playerLastDirection == 1:
+            playerIsLeft = False
+            playerIsRight = True
+
+    elif keys[pygame.K_DOWN] and playerPosY < BottomBorder:
         playerPosY += playerVel
+        if playerLastDirection == 0:
+            playerIsLeft = True
+            playerIsRight = False
+        elif playerLastDirection == 1:
+            playerIsLeft = False
+            playerIsRight = True
+    else:
+        playerIsLeft = False
+        playerIsRight = False
+        playerWalkCount = 0
 
     DrawGameWindow()
+    Player()
 
     # Checks all events occuring in PyGame
     for event in pygame.event.get():
